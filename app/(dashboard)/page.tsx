96c8fd441c645a0f27a02ddf8427ac0ce0699fd0
zhouxiao <zhouxiao@npn.sg>
2024-03-14 16:26:57 +0800

    feat: Search Querying

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -8,7 +8,7 @@ import { BoardList } from './_components/board-list'
 interface DashboardPageProps {
   searchParams: {
     search?: string
-    favorites?: boolean
+    favorites?: string
   }
 }
 
