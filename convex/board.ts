8dd54eba758287de84b078942c120b8950d692d6
zhouxiao <zhouxiao@npn.sg>
2024-03-19 16:58:15 +0800

    temp commit

--- convex/board.ts
+++ convex/board.ts
@@ -1,6 +1,6 @@
 import { v } from 'convex/values'
 
-import { mutation } from './_generated/server'
+import { mutation, query } from './_generated/server'
 
 const images = [
   '/placeholders/1.svg',
@@ -171,6 +171,19 @@ export const unfavorite = mutation({
     }
 
     await ctx.db.delete(existingFavorite._id)
+
+    return board
+  },
+})
+
+export const get = query({
+  args: { id: v.id('boards') },
+  handler: async (ctx, args) => {
+    const board = await ctx.db.get(args.id)
+
+    if (!board) {
+      throw new Error('Board not found')
+    }
 
     return board
   },
