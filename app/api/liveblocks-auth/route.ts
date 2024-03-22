b4e48809daf8985d4eb06646d88b15f7ee9cf25c
zhouxiao <zhouxiao@npn.sg>
2024-03-22 16:12:25 +0800

    feat: Room Authentication

---
+++ app/api/liveblocks-auth/route.ts
@@ -0,0 +1,44 @@
+import { api } from '@/convex/_generated/api'
+import { auth, currentUser } from '@clerk/nextjs'
+import { Liveblocks } from '@liveblocks/node'
+import { ConvexHttpClient } from 'convex/browser'
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
+
+const liveblocks = new Liveblocks({
+  secret:
+    'sk_dev_gW-Sq0Oi7Mt0Luo2HBmVkxjBghFGkdB2Anhqewi1GGsefDzPHPGdc-vib_TTKJZZ',
+})
+
+export async function POST(request: Request) {
+  const authorization = await auth()
+  const user = await currentUser()
+
+  if (!authorization || !user) {
+    return new Response('Unauthorized', { status: 403 })
+  }
+
+  const { room } = await request.json()
+  const board = await convex.query(api.board.get, { id: room })
+
+  if (board?.orgId !== authorization.orgId) {
+    return new Response('Unauthorized', { status: 403 })
+  }
+
+  const userInfo = {
+    name: user.firstName || 'Teammate',
+    picture: user.imageUrl,
+  }
+
+  console.log({ userInfo })
+
+  const session = liveblocks.prepareSession(user.id, { userInfo })
+
+  if (room) {
+    session.allow(room, session.FULL_ACCESS)
+  }
+
+  const { status, body } = await session.authorize()
+  console.log({ status, body }, 'ALLOWED')
+  return new Response(body, { status })
+}
