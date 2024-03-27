907544de7dadde296997a0babcab5383cb63ddf9
zhouxiao <zhouxiao@npn.sg>
2024-03-27 17:59:13 +0800

    temp commit

---
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useStorage } from '@/liveblocks.config'
+import { LayerType } from '@/types/canvas'
+import { memo } from 'react'
+
+interface LayerPreviewProps {
+  id: string
+  onLayerPointerDown: () => void // TODO: fix type
+  selectionColor?: string
+}
+
+export const LayerPreview = memo(
+  ({ id, onLayerPointerDown, selectionColor }: LayerPreviewProps) => {
+    const layer = useStorage((root) => root.layers.get(id))
+
+    if (!layer) {
+      return null
+    }
+
+    switch (layer.type) {
+      case LayerType.Rectangle:
+        return <div>Rectangle</div>
+      default:
+        console.warn('Unknown layer type')
+        return null
+    }
+  }
+)
+
+LayerPreview.displayName = 'LayerPreview'
